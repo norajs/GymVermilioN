c1be46241ea99f743b23b0f7dfd97eada6362172
norajs <norajs@users.noreply.github.com>
2017-08-04 11:45:56 -0400

    Add files via upload

---
+++ content.php
@@ -0,0 +1,97 @@
+ <!-- SECOND ROW -->
+         
+  <div id="second" class="row">
+    
+    <div id="one" class="col-sm-7">
+      <div  class="carousel slide" data-ride="carousel">
+        <div id="slides" class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img class="d-block img-fluid" src="pics\smaller\1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                    <img class="d-block img-fluid" src="pics\smaller\2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\3.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\4.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\5.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\6.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\7.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\8.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\9.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\10.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\11.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\12.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\13.jpg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="pics\smaller\14.jpg" alt="Third slide">
+            </div>
+        </div>
+        </div>
+    </div>
+      
+      
+      <div  id="two" class="col-sm">
+          <div id="fb" class="fb-page" data-href="https://www.facebook.com/Gym-VermilioN-1290761884304222" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Gym-VermilioN-1290761884304222" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Gym-VermilioN-1290761884304222">Gym VermilioN</a></blockquote></div>
+          <br/><br/>
+          <div id="follow">
+              <br/>
+          <div class="fb-follow" data-href="https://www.facebook.com/Gym-VermilioN-1290761884304222" data-width="25" data-height="25" data-layout="button" data-size="large" data-show-faces="true"></div>
+          <br/><br/>
+          <div class="fb-like" data-href="https://www.facebook.com/Gym-VermilioN-1290761884304222/" data-width="25" data-layout="standard" data-action="like" data-size="large" data-show-faces="false" data-share="true"></div>
+              <br/><br/>
+              </div>
+  
+    </div>
+   
+  </div>
+          
+<!-- THIRD ROW -->
+          
+ <div id="third" class="row">
+  <div class="col-sm">
+      <p id="text">Hivatalosan is megnyilott a Gym VermilioN, szeretettel varunk minden fiut es lanyt, 09:00 - 22:00-ig. A lehetosegek megtekinthetoek az oldalon levo fotoarchivumban, napi dij 5 leibe kerul valamint lehetoseg van havi berletet kotni, amik a kovetkezo tipusuak lehetnek: 30 napos 7/5-os fiuk resze 55 - lanyok reszere 50 lei, 30 nap 7/4 fiuk reszere 50 - lanyok reszere 45, 30 nap 7/3 fiuk reszere 45 - lanyok reszere 40 lei.
+      Privat uzenetben a facebook oldalunkon vagy akar a kovetkezo szamra klikkelve telefonon <a href="tel:+40753483987">+40 753 483 987</a>, barmikor elerhetoek vagyunk.
+      Remelem hamarosan latjuk egymast, udv Gym VermilioN.</p>   
+  </div> 
+</div>
+          
+<!-- FOURTH ROW-->
+                  
+<div id="fourth" class="row">
+  <div class="col-sm">
+      
+      <div class="fb-video" data-href="https://www.facebook.com/brightside/videos/415910292090187" data-width="1500" data-show-text="false"><blockquote cite="https://www.facebook.com/brightside/videos/415910292090187/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/brightside/videos/415910292090187/">Why exercise doesn&#039;t work</a><p>It&#039;s vital to do everything right goo.gl/bpW5pE☝️</p>Posted by <a href="https://www.facebook.com/brightside/">Bright Side</a> on Friday, 10 February 2017</blockquote></div>
+  </div> 
+</div>         
+<div id="fifth" class="row">
+  <div class="col-sm">    
+      <iframe id= "map" src="https://embed.waze.com/iframe?zoom=30&lat=47.354&lon=22.823" width="300" height="400"></iframe>
+  </div>
+  
+</div>
+          
+          
+
+          
---
+++ header.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+      
+      <link rel="stylesheet" href="style.css">
+      
+       <link rel="icon" type="image/png" href="favlogo.png" sizes="32x32" />
+        
+        <title>Gym Vermilion</title>
+  </head>
+  <body>
+      
+      <!-- follow -->
+
+
+      
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+      
+      <!-- like and share -->
+      
+      
+      
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+      
+      
+      <!-- video -->
+      
+      
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+      
+      
+      
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+ 
+<div class="container">
+          
+<!-- FIRST ROW --> 
+          
+          
+  <div id="first" class="row">
+    <div class="col-sm">
+      <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+            <img src="logofull.png" width="140" height="40" alt="">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link" ></a>
+        <a class="nav-item nav-link" data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">Árlista</a>
+ 
+        <a class="nav-item nav-link" data-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">Nyitvatartas</a>
+  
+        <a class="nav-item nav-link" data-toggle="collapse" href="#collapseExample3" aria-expanded="false" aria-controls="collapseExample3">Elerhetosegek</a>
+    
+        <div class="collapse" id="collapseExample1">
+            <div id="arlista" class="card card-block">
+                Napi dij 5 lei <br/>
+                30 napos 7/5-os fiuk reszere 55 lei <br/>
+                30 napos 7/5-os lanyok reszere 50 lei <br/>
+                30 napos 7/4-os fiuk reszere 50 lei <br/>
+                30 napos 7/4-os lanyok reszere 45 lei <br/>
+                30 napos 7/3-os fiuk reszere 45 lei <br/>
+                30 napos 7/3-os lanyok reszere 40 lei <br/>
+    
+            </div>
+        </div>
+        
+        <div class="collapse" id="collapseExample2">
+            <div id="arlista" class="card card-block">
+                Minden nap 9:00 - 22:00 <br/>
+            </div>
+        </div>
+        
+        <div class="collapse" id="collapseExample3">
+            <div id="arlista" class="card card-block">
+    
+                Str. Garoafelor, nr.5 <br/>
+                457330 Sarmasag, Salaj <br/>
+                <a href="tel:+40753483987">Tel: +40 753 483 987</a> <br/>
+    
+            </div>
+        </div>
+ 
+        </div>
+        </div>
+    </nav>
+    </div> 
+  </div>
+          
+          
+          
+
+        
+          
+          
+          
+          
